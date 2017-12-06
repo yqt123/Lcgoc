6c48d019fbf3623555ead840acf0df1d49c65660
cheng <864083509@qq.com>
2017-12-06 23:46:07 +0800

    登录

--- Lcgoc.Web/Areas/Admin/Controllers/AccountController.cs
+++ Lcgoc.Web/Areas/Admin/Controllers/AccountController.cs
@@ -26,7 +26,7 @@ namespace Lcgoc.Web.Areas.Admin.Controllers
 
         [HttpPost]
         [ControlRightAllowAnonymousAttribute]
-        //防止了跨站攻击
+        //防止跨站攻击
         [ValidateAntiForgeryToken]
         public ActionResult Login(LoginModel model, string returnUrl)
         {
@@ -35,21 +35,19 @@ namespace Lcgoc.Web.Areas.Admin.Controllers
                 string userId = string.Empty;
                 if (bll.IsAuthorized(model.UserName, model.Password, ref userId))
                 {
-                    Services.AuthorizationManager.SetTicket(model.RememberMe, 1, userId, model.UserName);
-
-                    //var user = bll.GetUser(userId);
-                    //System.Web.HttpContext.Current.Session["admin_cookies"] = user;
-                    ////创建身份验证票据 User.Identity.Name=model.UserName
-                    //FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-                    //if (!string.IsNullOrEmpty(returnUrl) && HttpUtility.UrlDecode(returnUrl).Split('/').Length == 2)
-                    //{
-                    //    var controllerAction = HttpUtility.UrlDecode(returnUrl).Split('/');
-                    //    return RedirectToAction(controllerAction[1], controllerAction[0]);
-                    //}
-                    //else
-                    //{
-                    //    return RedirectToAction("Index", "Home");
-                    //}
+                    var user = bll.GetUser(userId);
+                    //
+                    System.Web.HttpContext.Current.Session[WebConfig.LoginCookieName] = user;
+                    
+                    if (!string.IsNullOrEmpty(returnUrl) && HttpUtility.UrlDecode(returnUrl).Split('/').Length == 2)
+                    {
+                        var controllerAction = HttpUtility.UrlDecode(returnUrl).Split('/');
+                        return RedirectToAction(controllerAction[1], controllerAction[0]);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
                 }
             }
             ModelState.AddModelError("", "提供的账号或密码不正确。");
--- Lcgoc.Web/Services/AuthorizationManager.cs
+++ Lcgoc.Web/Services/AuthorizationManager.cs
@@ -26,5 +26,20 @@ namespace Lcgoc.Web.Services
             HttpCookie authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrytedTicket);
             HttpContext.Current.Response.Cookies.Add(authCookie);
         }
+
+        /// <summary>
+        /// 设置后台管理平台的登录凭证，考虑到一个系统可能多个登录的情况，没有使用框架自带的FormsAuthentication认证（微信平台已经使用）
+        /// </summary>
+        /// <param name="remeberMe"></param>
+        /// <param name="identity"></param>
+        public static void SetAdminTicket(bool remeberMe, string identity)
+        {
+            System.Web.HttpContext.Current.Session[WebConfig.LoginCookieName] = identity;
+            if (remeberMe)
+            {
+
+            }
+        }
+
     }
 }
